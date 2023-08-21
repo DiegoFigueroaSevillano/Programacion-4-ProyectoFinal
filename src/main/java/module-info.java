37ba42266d1bdd4ff1488bd5410384b36893974f
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2023-08-20 20:36:43 -0400

    config: JavaFX with Graddle added

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.programacion4proyectofinal {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.programacion4proyectofinal to javafx.fxml;
+    exports com.example.programacion4proyectofinal;
+}
